cfd25195ef9d419ae47bafc4fc75a80f0eca995e
ronitgamer <100210277+ronitgamer@users.noreply.github.com>
2022-02-22 18:47:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+song="";
+
+function setup(){
+canvas=createCanvas(600,500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+image(video,0,0,600,500) ;
+
+}
+
+function preload(){
+
+song=loadSound("song2.mp3");
+
+
+}
+function play(){
+song.play();
+
+}
